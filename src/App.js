a9d1a40b3f402f138b7c8bcdf851e4335dc0b518
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-22 16:12:11 -0600

    feat: iterar una lista de elementos uso del key

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Greeting from './components/greeting/greeting';
 import Poem from './components/poem/poem';
 import Generation from './components/greeting/generation';
+import { Navbar } from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Greeting />
       <Poem/>
       <Generation/>
+      {/* footer */}
     </div>
   );
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,29 @@
 //snipet traido como rafc
 
+//const pages = ["Home", "Users", "About", "Register"];
+const pages = [
+    {name: "Home", url: "/"}, 
+    {name: "Users", url: "/"}, 
+    {name: "Counter", url: "/"}, 
+    {name: "About", url: "/"}, 
+    {name: "Register", url: "/"}, 
+    ];
+/* 
+ Cuando se renderiza una lista de elementos, es importante
+ proporcionar una prop "key" única a cada elemento de la lista.
+ Esto permite a React realizar una actualización eficiente
+ y optimizada de la interfaz de usuario cuando cambia el
+ contenido de la lista.
+
+*/
+
 export const Navbar = () => {
   return (
-    <div>
+    <>
         <h4>CH30</h4>
-    </div>
+        <ul>
+            { pages.map( page => <li key={page.name}>{page.name}</li>) }
+        </ul>
+    </>
   )
 }
