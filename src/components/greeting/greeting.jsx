5277119949c7cf11e8f683400cb02b881adff4a5
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-22 14:16:16 -0600

    feat: componentes

---
+++ src/components/greeting/greeting.jsx
@@ -0,0 +1,16 @@
+
+
+/* Functional component
+
+Los componentes funcionales son funciones de Javascript
+que devuelven JSX. Estas funciones tienen una sintaxis
+más simple y concisa
+
+*/
+
+export default function Greeting(){
+    return (<div>
+        <h1>Felicidades Generation por su 8vo Aniversario</h1>
+        <h2>Pueden llegar a la pachanga</h2>
+        </div>);
+}
---
+++ src/components/poem/poem.jsx
@@ -0,0 +1,11 @@
+export default function Poem(){
+    return (<p>
+        Te espero cuando la noche se haga día,<br></br>
+        suspiros de esperanzas ya perdidas.<br></br>
+        No creo que vengas, <i>lo sé,</i> <br></br>
+        sé que no vendrás. <br></br>
+        Sé que la distancia te hiere,<br></br>
+        sé que las noches son más frías,<br></br>
+        <b>Sé que ya no estás.</b>
+        </p>);
+}
