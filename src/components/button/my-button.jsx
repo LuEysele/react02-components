99e9d6511d5ddc325a84f1a44b578200099a4156
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-24 11:29:16 -0600

    ci: aplicar estilos con operador ternario

--- src/components/button/my-button.jsx
+++ src/components/button/my-button.jsx
@@ -1,9 +1,35 @@
+import { useEffect } from "react";
+
+const buttonStyle = {
+    color: "red",
+    fontFamily: "verdana",
+    fontSize: 18
+};
+const resetStyle = {
+    color: "green",
+    fontFamily: "calibri",
+    fontSize: 24
+};
+
+export const MyButton = ({ onClick: handleOnClick, children }) => {
+    console.log(`Estoy dentro del componente button de ${children}`);
+    /*
+     el hook useEffect es utilizado para trabajar con efectos secuendarios enm
+     componentes funcionales.
+     Podemos controlar las acciones que ocurren después de que el componente se renderiza,
+     como realizar llamadas a APIs, suscripciones a eventos, manipulación del DOM.
+     useEffect( ()=> , [] );
+    */
+    const maxValue = 0;
+    useEffect(() => {
+        console.log("Me renderizo dentro de useEffect ")
+
+    }, []);
 
 
-export const MyButton = ({onClick, children}) => {
     return (
         <>
-            <button onClick ={onClick} >{children}</button>
+            <button onClick={handleOnClick} style={children === "Reset" ? resetStyle : buttonStyle} >{children}</button>
         </>
-    )
-}
+    );
+};
--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MyButton } from "../button/my-button";
 
+let maxValue = 0;
 
 export const Counter = () =>{
-    console.log("Estoy dentro del componente counter")
+    console.log("Estoy dentro del componente counter");
 
     // hook useState
     // useState permite a los componentes gestionar su propio estado.
@@ -24,12 +25,22 @@ export const Counter = () =>{
         console.log("Contador: " + counterValue)
     }
 
+    if ( counterValue === 90 )
+    maxValue = 90;
+
+
+  useEffect(() => {
+    console.log(`Estoy dentro del useEffect de button`);
+    console.log("Has llegado al máximo valor");
+  }, [maxValue] );
+
+
     return(
         <>
         <p>Contador: {counterValue}</p>
         <MyButton onClick={handleDecrement}>Decremento</MyButton>
-        <MyButton onClick={handleIncrement}>Incremento</MyButton>
         <MyButton onClick={(event)=> setCounter(100)}>Reset</MyButton>
+        <MyButton onClick={handleIncrement}>Incremento</MyButton>
        {/*  <button onClick={handleDecrement}>Decrementar</button>
         <button onClick={handleIncrement}>Incrementar</button>
         <button onClick={(event)=> setCounter(100)}>Reset</button> */}
